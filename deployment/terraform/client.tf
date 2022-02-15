c8e515e247159dadd862975f2225936fb4cff385
Adam Walford <>
2022-02-15 18:42:26 +0000

    allow refresh token auth

--- deployment/terraform/client.tf
+++ deployment/terraform/client.tf
@@ -5,5 +5,5 @@ resource "aws_cognito_user_pool_client" "client" {
   user_pool_id = aws_cognito_user_pool.revlet_userpool.id
 
   generate_secret     = false
-  explicit_auth_flows = ["ALLOW_USER_SRP_AUTH"]
+  explicit_auth_flows = ["ALLOW_REFRESH_TOKEN_AUTH", "ALLOW_USER_SRP_AUTH"]
 }
